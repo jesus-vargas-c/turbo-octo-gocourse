615e6a40f4fca2d18f1e9f813bf7890bd50b6a80
Jesus Vargas <vargas.jesus1984@gmail.com>
2023-02-22 22:02:46 -0600

    solution for first exercise

---
+++ section1/exercises/practice1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Practice1() error {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	return nil
+}
